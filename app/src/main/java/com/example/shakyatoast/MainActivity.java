514f1d059b535ebebdcfb800ea668268ad0eb7e4
Riley Ho <riley@rileychh.dev>
2024-11-01 15:18:26 +0800

    feat: complete lab

--- app/src/main/java/com/example/shakyatoast/MainActivity.java
+++ app/src/main/java/com/example/shakyatoast/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.shakyatoast;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -28,12 +31,43 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        binding.button.setOnClickListener(view -> {
+            final AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("請選擇功能")
+                    .setMessage("請更具下方按鈕選擇要顯示的物件")
+                    .setNeutralButton("取消", (dialogInterface, i) ->
+                            Toast.makeText(MainActivity.this, "取消", Toast.LENGTH_SHORT).show()
+                    )
+                    .setNegativeButton("自定義 Toast", (dialogInterface, i) -> showCustomDialog())
+                    .setPositiveButton("顯示 list", (dialogInterface, i) -> showListDialog())
+                    .create();
+            dialog.show();
+        });
+    }
+
+    private void showCustomDialog() {
         Toast toast = new Toast(this);
+        ToastBinding toastBinding = ToastBinding.inflate(getLayoutInflater());
         toast.setGravity(Gravity.TOP, 0, 50);
         toast.setDuration(Toast.LENGTH_SHORT);
-
-        ToastBinding toastBinding = ToastBinding.inflate(getLayoutInflater());
         toast.setView(toastBinding.getRoot());
         toast.show();
+    }
+
+    private void showListDialog() {
+        final String[] list = {
+                "message1",
+                "message2",
+                "message3",
+                "message4",
+                "message5",
+        };
+        AlertDialog listDialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("使用 List 呈現")
+                .setItems(list, ((dialogInterface, i) -> {
+                    Toast.makeText(MainActivity.this, "您選擇的是 " + list[i], Toast.LENGTH_SHORT).show();
+                }))
+                .create();
+        listDialog.show();
     }
 }
